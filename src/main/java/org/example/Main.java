e711bc26d4ff4acecdfc6016f3fc9121c7ed14b9
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-11-09 12:39:35 +0100

    Use @AllArgsConstructor

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        DogSchool mySchool = new DogSchool();
+        DogSchool mySchool = new DogSchool(3, 7);
         mySchool.addMember();
         mySchool.setDogs(4);
         mySchool.addMember();
